8145d746b489c2b11e89ba66ca4b51a03dc1b051
Sebastián <129956847+SebastianCh81@users.noreply.github.com>
2023-05-14 19:35:18 -0300

    agregado de lo 3 primeros metodos y metodos en clase feria

--- src/a/a/Feria.java
+++ src/a/a/Feria.java
@@ -1,4 +1,5 @@
 package a.a;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,81 +12,149 @@ import java.util.Set;
 import unlam.edu.ar.pb2.parcial.src.Entrada;
 import unlam.edu.ar.pb2.parcial.src.Persona;
 import unlam.edu.ar.pb2.parcial.src.Stant;
+import unlam.edu.ar.pb2.parcial.src.TipoPersona;
 
 public class Feria {
 	// attributes
-		private String nombreDeLaFeria;
-		private String ubicacion;
-		private Integer tamaño;
-		Boolean pagoentradas = false;
-		LocalDate fechaActual = LocalDate.now();
-		DayOfWeek diaDeLaSemana = fechaActual.getDayOfWeek();
+	private String nombreDeLaFeria;
+	private String ubicacion;
+	private Integer tamaño;
+	Boolean pagoentradas = false;
+	LocalDate fechaActual = LocalDate.now();
+	DayOfWeek diaDeLaSemana = fechaActual.getDayOfWeek();
 
-		// collections
-		private ArrayList<Persona> personas;
-		private Set<Entrada> entradas;
-		private Map<Persona, Entrada> asignacionEntrada = new HashMap<>();
-		private Set<Stant> stants;
+	// collections
+	private ArrayList<Persona> personas;
+	private Set<Entrada> entradas;
+	private Map<Persona, Entrada> asignacionEntrada = new HashMap<>();
+	private Set<Stant> stants;
 
-		// constructor
-		public Feria(String nombreDeLaFeria, String ubicacion, Integer tamaño) {
-			super();
-			this.nombreDeLaFeria = nombreDeLaFeria;
-			this.ubicacion = ubicacion;
-			this.tamaño = tamaño;
-			this.personas = new ArrayList<>();
-			this.entradas = new HashSet<>();
-			this.stants = new HashSet<>();
+	// constructor
+	public Feria(String nombreDeLaFeria, String ubicacion, Integer tamaño) {
+		super();
+		this.nombreDeLaFeria = nombreDeLaFeria;
+		this.ubicacion = ubicacion;
+		this.tamaño = tamaño;
+		this.personas = new ArrayList<>();
+		this.entradas = new HashSet<>();
+		this.stants = new HashSet<>();
+	}
+
+	// getters ands setters
+
+	public String getNombreDeLaFeria() {
+		return nombreDeLaFeria;
+	}
+
+	public void setNombreDeLaFeria(String nombreDeLaFeria) {
+		this.nombreDeLaFeria = nombreDeLaFeria;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public Integer getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(Integer tamaño) {
+		this.tamaño = tamaño;
+	}
+
+	public ArrayList<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(ArrayList<Persona> personas) {
+		this.personas = personas;
+	}
+
+	public Set<Entrada> getEntradas() {
+		return entradas;
+	}
+
+	public void setEntradas(Set<Entrada> entradas) {
+		this.entradas = entradas;
+	}
+
+	public Map<Persona, Entrada> getAsignacionEntrada() {
+		return asignacionEntrada;
+	}
+
+	public void setAsignacionEntrada(Map<Persona, Entrada> asignacionEntrada) {
+		this.asignacionEntrada = asignacionEntrada;
+	}
+
+	// methods
+	// se realiza la venta de una entrada pero con las siguientes condiciones:
+	// 1- si es un alumno y ademas son los dias viernes, sabados y domingos debe
+	// pagar la entrada
+	// 2- si es una docente los dias domingos no debe pagar la entrada
+	// 3- si no es ninguna de las anteriores debe pagar la entrada
+	// si se requiere se puede modificar para mas tipos de personas.
+	public void venderEntrada(Persona persona, Entrada entrada, DayOfWeek diaSemana) {
+		Double pago = 0.0;
+		for (Entrada entrada1 : entradas) {
+			if (entrada1.equals(entrada)) {
+				if (persona.getTipoPersona() == TipoPersona.ALUMNO) {
+					if (diaDeLaSemana == DayOfWeek.FRIDAY || diaDeLaSemana == DayOfWeek.SATURDAY
+							|| diaDeLaSemana == DayOfWeek.SUNDAY) {
+						getAsignacionEntrada().put(persona, entrada);
+						pago = persona.getDinero() - entrada.getPrecioEntrada();
+						persona.setDinero(pago);
+					}
+
+				} else if (persona.getTipoPersona() == TipoPersona.DOCENTE && diaSemana == DayOfWeek.SUNDAY) {
+					getAsignacionEntrada().put(persona, entrada);
+				} else {
+					getAsignacionEntrada().put(persona, entrada);
+					pago = persona.getDinero() - entrada.getPrecioEntrada();
+					persona.setDinero(pago);
+				}
+			}
 		}
 
-		// getters ands setters
+	}
 
-		public String getNombreDeLaFeria() {
-			return nombreDeLaFeria;
-		}
+	public void agregarPersona(Persona persona) {
+		this.personas.add(persona);
 
-		public void setNombreDeLaFeria(String nombreDeLaFeria) {
-			this.nombreDeLaFeria = nombreDeLaFeria;
-		}
+	}
 
-		public String getUbicacion() {
-			return ubicacion;
-		}
+	public Integer obtenerCantidadPersonas() {
+		return personas.size();
 
-		public void setUbicacion(String ubicacion) {
-			this.ubicacion = ubicacion;
-		}
+	}
 
-		public Integer getTamaño() {
-			return tamaño;
-		}
+	public void agregarEntrada(Entrada entrada) {
+		this.entradas.add(entrada);
 
-		public void setTamaño(Integer tamaño) {
-			this.tamaño = tamaño;
-		}
+	}
 
-		public ArrayList<Persona> getPersonas() {
-			return personas;
-		}
+	public Integer obtenerCantidadDeEntradas() {
+		return this.entradas.size();
 
-		public void setPersonas(ArrayList<Persona> personas) {
-			this.personas = personas;
-		}
+	}
 
-		public Set<Entrada> getEntradas() {
-			return entradas;
-		}
+	public void agregarStant(Stant stant) {
+		this.stants.add(stant);
 
-		public void setEntradas(Set<Entrada> entradas) {
-			this.entradas = entradas;
-		}
+	}
 
-		public Map<Persona, Entrada> getAsignacionEntrada() {
-			return asignacionEntrada;
-		}
+	public Boolean verificarQueUnMenorTengaAUnAdulto(Persona persona) {
+		Boolean requiereAcompaniante = false;
+		for (Persona persona1 : personas) {
+			if (persona1.getEdad() <= 12) {
+				return requiereAcompaniante = true;
 
-		public void setAsignacionEntrada(Map<Persona, Entrada> asignacionEntrada) {
-			this.asignacionEntrada = asignacionEntrada;
+			}
 		}
- 
+		return requiereAcompaniante;
+	}
+
 }
--- test/a/a/TestFeriadelLibro.java
+++ test/a/a/TestFeriadelLibro.java
@@ -2,12 +2,100 @@ package a.a;
 
 import static org.junit.Assert.*;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 
 import org.junit.Test;
 
-public class TestFeriadelLibro {
+import unlam.edu.ar.pb2.parcial.src.Entrada;
+import unlam.edu.ar.pb2.parcial.src.Feria;
+import unlam.edu.ar.pb2.parcial.src.Persona;
+import unlam.edu.ar.pb2.parcial.src.TipoPersona;
 
+public class TestFeriadelLibro {
+	@Test // Sebastian
+	public void queSePuedaAgregarUnaPersonaAlaFeria() {
+		String nombreDeLaFeria = "FeriaDelLibro";
+		String ubicacionFeria = "La Rural";
+		Integer tamaño = 2000;
+		Feria feria = new Feria(nombreDeLaFeria, ubicacionFeria, tamaño);
+
+		String nombre = "Juan";
+		String apellido = "Perez";
+		Integer nroDni = 42351234;
+		Integer edad = 30;
+		TipoPersona tipoPersona = TipoPersona.ADULTO;
+		Double dinero = 5000.0;
+		Persona persona = new Persona(nombre, apellido, nroDni, edad, tipoPersona, dinero);
+
+		// se agrega una persona al array para luego verificar su existencia en la misma
+		feria.agregarPersona(persona);
+		Integer valorObtenido = feria.obtenerCantidadPersonas();
+		Integer valorEsperado = 1;
+		assertEquals(valorEsperado, valorObtenido);
+
+	}
+
+	@Test // Sebastian
+	public void queSePuedaAgregarUnaEntradaAlaFeria() {
+		String nombreDeLaFeria = "FeriaDelLibro";
+		String ubicacionFeria = "La Rural";
+		Integer tamaño = 2000;
+		Feria feria = new Feria(nombreDeLaFeria, ubicacionFeria, tamaño);
+
+		Integer numeroentrada = 1111;
+		Boolean sinUsar = true;
+		Double precioEntrada = 2000.0;
+		Entrada entrada = new Entrada(numeroentrada, sinUsar, precioEntrada);
+
+		// se agrega una entrada al array para luego verificar su existencia en la misma
+		feria.agregarEntrada(entrada);
+		Integer valorObtenido = feria.obtenerCantidadDeEntradas();
+		Integer valorEsperado = 1;
+
+		assertEquals(valorEsperado, valorObtenido);
+
+	}
+
+	@Test // Sebastian
+	public void queLaFeriaLeVendaUnaEntradaAUnaPersona() {
+		String nombreDeLaFeria = "FeriaDelLibro";
+		String ubicacionFeria = "La Rural";
+		Integer tamaño = 2000;
+		Feria feria = new Feria(nombreDeLaFeria, ubicacionFeria, tamaño);
+
+		// se "muestra" el dia de la semana con respecto a la fecha actual
+		LocalDate fechaActual = LocalDate.now();
+		DayOfWeek diaSemana = fechaActual.getDayOfWeek();
+
+		String nombre = "Juan";
+		String apellido = "Perez";
+		Integer nroDni = 42351234;
+		Integer edad = 30;
+		TipoPersona tipoPersona = TipoPersona.ADULTO;
+		Double dinero = 5000.0;
+		Persona persona = new Persona(nombre, apellido, nroDni, edad, tipoPersona, dinero);
+
+		feria.agregarPersona(persona);
+
+		Integer numeroentrada = 1111;
+		Boolean sinUsar = true;
+		Double precioEntrada = 2000.0;
+		Entrada entrada = new Entrada(numeroentrada, sinUsar, precioEntrada);
+
+		feria.agregarEntrada(entrada);
+
+		feria.venderEntrada(persona, entrada, diaSemana);
+
+		// Verificar si la persona tiene una entrada
+		boolean verficarSiPersonaTieneEntrada = feria.getAsignacionEntrada().containsKey(persona);
+		assertTrue(verficarSiPersonaTieneEntrada);
+
+		// Verificar que la entrada es la correcta
+		assertEquals(numeroentrada, feria.getAsignacionEntrada().get(persona).getNumeroEntrada());
+
+	}
 }
